877552c2da700ef089e55c20e4db221c6bbdc4d3
Bruno Filippini <brunofilippini94@gmail.com>
2022-02-09 18:37:26 -0300

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import { Pets } from "./pages/Pets";
 
